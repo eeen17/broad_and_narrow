9c8786db1a74db3377a745a63550482b948af489
zeynebnk <70820835+zeynebnk@users.noreply.github.com>
2025-05-12 16:30:15 -0700

    Update eval_pipe.py

--- eval_pipe.py
+++ eval_pipe.py
@@ -15,11 +15,11 @@ def main():
     args = parser.parse_args()
 
     if args.n_digits == 2:
-        q_main(f'arithmetic/data/0shot/base{args.base}.txt', args.base, args.model_name, f'output_{args.base}_{args.model_name}.txt', cot=args.cot, n_shots=0, size=args.size)
+        q_main(f'arithmetic/data/0shot/base{args.base}.txt', args.base, args.model_name, f'output_{args.base}.txt', cot=args.cot, n_shots=0, size=args.size)
     elif args.n_digits == 3:
-        q_main(f'arithmetic/data/0shot_3digits/base{args.base}.txt', args.base, args.model_name, f'output_{args.base}_{args.model_name}.txt', cot=args.cot, n_shots=0, size=args.size)
+        q_main(f'arithmetic/data/0shot_3digits/base{args.base}.txt', args.base, args.model_name, f'output_{args.base}.txt', cot=args.cot, n_shots=0, size=args.size)
     elif args.n_digits == 4:
-        q_main(f'arithmetic/data/0shot_4digits/base{args.base}.txt', args.base, args.model_name, f'output_{args.base}_{args.model_name}.txt', cot=args.cot, n_shots=0, size=args.size)
+        q_main(f'arithmetic/data/0shot_4digits/base{args.base}.txt', args.base, args.model_name, f'output_{args.base}.txt', cot=args.cot, n_shots=0, size=args.size)
     
     e_main(f'output_{args.base}_{args.model_name}.txt', args.base)
 main()
