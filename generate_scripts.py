5ecae3a001306344e02dbd39a2e0243828e11cf8
zeynebnk <70820835+zeynebnk@users.noreply.github.com>
2025-06-09 18:18:25 -0700

    Create generate_scripts.py

---
+++ generate_scripts.py
@@ -0,0 +1,47 @@
+import os
+
+os.makedirs("ft_data", exist_ok=True)
+
+bases = [8, 9, 10]
+cots = ["--cot", ""]
+eval_n_digits = [2, 3, 4]
+train_n_digits = [2]
+train_types = ["ft_lora"]
+eval_checkpoints = [60, 50, 40, 30, 20, 10]
+
+command_file = "all_commands.sh"
+
+
+## datagen code
+for base in bases:
+    for n_digits in train_n_digits:    
+        command = f"python sample_pipe.py --base {base} --n_digits {2} --n_samples {500}"
+        with open(command_file, "a") as f:
+            f.write(command + "\n")
+
+
+## training code
+for train_type in train_types:
+    for base in bases:
+        for cot in cots:
+            for n_digits in train_n_digits:
+                command = f"python {train_type}.py --base {base} --model_path unsloth/Phi-4 {cot} --data_file ft_data/data_ft_{base}_{n_digits}.txt"
+                with open(command_file, "a") as f:
+                    f.write(command + "\n")
+                
+## eval code
+models = os.listdir("outputs")
+for checkpoint in eval_checkpoints:
+    for base in bases:
+        for n_digits in eval_n_digits:
+            for model in models:
+                commands = f"python eval_pipe.py --base {base} --model_name outputs/{model}/checkpoint-{checkpoint} --size 300 --n_digits {n_digits}"
+                with open(command_file, "a") as f:
+                    f.write(command + "\n")
+# and baseline evals
+for base in bases:
+    for n_digits in eval_n_digits:
+        commands = f"python eval_pipe.py --base {base} --model_name unsloth/Phi-4 --size 300 --n_digits {n_digits}"
+        with open(command_file, "a") as f:
+            f.write(command + "\n")
+                
