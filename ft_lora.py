bf822c77a2af989f9c38f4d010e2d6dc6af03589
zeynebnk <70820835+zeynebnk@users.noreply.github.com>
2025-07-09 12:27:48 -0700

    <3

--- ft_lora.py
+++ ft_lora.py
@@ -32,7 +32,6 @@ def load_model(model_path, chat_template, r=16, lora_alpha=32, peft_path=None):
         lora_dropout = 0, 
         bias = "none",
         use_gradient_checkpointing = "unsloth",
-        random_state = 3407
         )
 
     tokenizer = get_chat_template(tokenizer, chat_template = chat_template)
@@ -60,28 +59,21 @@ def prep_dataset(dataset, tokenizer):
 from arithmetic.query import templatize, answer
 from arithmetic.eval import get_label
 import json
-def get_dataset(base, cot, n_digits, data_file=None, oai=False):
+def get_dataset(base, cot, n_digits, data_file=None):
     if data_file is None:
         data_file = f'ft_data/data_ft_{base}_{n_digits}.txt'
     x = [line.strip() for line in open(data_file)]
+    
     if cot:
         y = [answer(line, base) for line in x]
     else:
         y = ['\\boxed{'+ str(get_label(line, base))+'}' for line in x]
     x = [templatize(line, base, cot) for line in x]
-    m_head = "conversations"
-    if oai:
-        m_head = "messages"
-    if not oai:
-        with open('tmp.json', 'w') as f:
-            json.dump([{"conversations":[{'role':'user','content':q},{'role':'assistant','content':a}]} for q,a in zip(x,y)], f)
-            dataset = load_dataset('json', data_files='tmp.json')
-    else:
-        with open('tmp.jsonl', 'w') as f:
-            for q, a in zip(x, y):
-                json.dump({"messages":[{'role':'user','content':q},{'role':'assistant','content':a}]}, f)
-                f.write("\n")
-        dataset = None
+
+    with open('tmp.json', 'w') as f:
+        json.dump([{"conversations":[{'role':'user','content':q},{'role':'assistant','content':a}]} for q,a in zip(x,y)], f, indent=2)
+    dataset = load_dataset('json', data_files='tmp.json')
+    
     return dataset
 
 
@@ -114,9 +106,8 @@ def train_model(model, tokenizer, base, cot, n_digits,data_file,res_only=True):
             bf16 = is_bfloat16_supported(),
             logging_steps = 1,
             optim = "adamw_8bit",
-            weight_decay = 0.01,
+            # weight_decay = 0.01,
             lr_scheduler_type = "linear",
-            seed = 3407,
             output_dir = "outputs/" + name,
             save_strategy = "steps",
             save_steps = 5
@@ -132,26 +123,6 @@ def train_model(model, tokenizer, base, cot, n_digits,data_file,res_only=True):
     return trainer_stats
 
 
-def train_model_oai(model, base, cot, n_digits,data_file):
-  dataset = get_dataset(base, cot, n_digits, data_file, oai=True)
-  client = OpenAI()
-  
-  f = client.files.create(
-    file=open('tmp.jsonl', "rb"),
-    purpose="fine-tune"
-  )
-
-  m = client.fine_tuning.jobs.create(
-      training_file=f.id,
-      model=model,
-      hyperparameters = {'n_epochs':1, 'batch_size':16}
-  )
-  
-  with open(f"outputs/oai_models", "a") as f:
-      f.write(f"test_run_{base}_cot_{cot}_n_digits_{n_digits}"  + ": " + str(m.id))
-  
- 
-
 def main(args):
     print(args)
     base = args.base
@@ -159,14 +130,11 @@ def main(args):
     cot = args.cot
     n_digits = args.n_digits
     chat_template = "phi-4"
-    r, lora_alpha = 64, 128
 
-    if "gpt" not in args.model_path:
-        model, tokenizer = load_model(model_path, chat_template, r, lora_alpha)
-        print("training model...")
-        train_stats = train_model(model, tokenizer, base, cot, n_digits, data_file=args.data_file, res_only=True)
-    else:
-        train_stats = train_model_oai(args.model_path, base, cot, n_digits, data_file=args.data_file)
+    model, tokenizer = load_model(model_path, chat_template)
+    print("training model...")
+    train_stats = train_model(model, tokenizer, base, cot, n_digits, data_file=args.data_file, res_only=True)
+    
     print(train_stats)
 
 
